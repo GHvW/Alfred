b0d052469eedeb3d6877d193129d3ee910eef730
Garrett van Wageningen <ghvanw283@gmail.com>
2021-01-31 21:45:36 -0600

    Create readline.cljs

---
+++ src/main/alfred/app/nodjs_wrappers/readline.cljs
@@ -0,0 +1,26 @@
+(ns alfred.app.nodjs-wrappers.readline
+  (:require 
+   ["readline" :as readline]
+   ["fs" :as fs]
+   [cljs.core.async :refer [go chan <! put!]]))
+
+
+(defn read-line
+  [path]
+  (let [out-chan (chan)
+        line-reader (.createInterface 
+                     readline 
+                     #js {:input (.createReadStream fs path)})]
+    (.on line-reader "line" (fn [line] (put! out-chan line)))
+    out-chan))
+
+
+(comment
+
+  (let [line-chan (read-line "C:/Users/ghvw/Desktop/artshortlist.txt")]
+    (go
+      (loop []
+        (println (<! line-chan))
+        (recur))))
+  
+  (+ 2 2))
